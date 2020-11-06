93514a244d252158a693dee34af315b74a2d2158
Student   DOWDELL James <05607@farnborough.ac.uk>
2020-11-06 10:56:03 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World, not generic at all.");
+        String iAmReNameable = "hi";
+        System.out.println(iAmReNameable.length());
+    }
+}
